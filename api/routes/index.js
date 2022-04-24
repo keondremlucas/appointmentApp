89d836e1b637f73b2a8a9385ed1d79ae56201ce9
Keondre Lucas <keondre.m.lucas@my.ccsu.edu>
2022-04-23 21:57:59 -0400

    Added more api functions - remove/delete all, adding statuses to successful requests

--- api/routes/index.js
+++ api/routes/index.js
@@ -15,34 +15,48 @@ router.get('/', (req, res, next) => {
     });
 
 
-router.post('/', (req, res, next) => {
-    const {appointmentDate, name, email} = req.body;
-    const appointment =  {appointmentDate, name, email};
-    const collection = req.app.locals[config.dbCollection];
-    if(!appointmentDate || !name || !email) {
-        return res.status(400).json({
-            message:'All fields are required'
-        })
-    }
-    else
+router.post('/', (req, res, next) => 
     {
-    collection.insertOne({
-            appointment
-        })
+        const {appointmentDate, name, email} = req.body;
+        const appointment =  {appointmentDate, name, email};
+        const collection = req.app.locals[config.dbCollection];
+        if(!appointmentDate || !name || !email) {
+            return res.status(400).json({
+                message:'All fields are required'
+            });
+        }
+        else
+        {
+            try
+            {
+                collection.insertOne({appointment});
+                const result = res.status(200).json(appointment);
+                res.send(result + 
+                'Appointment created successfully');
+            } 
+            catch
+            {
+                (error => res.send(error));
+            }
+        }
+    });
+
+
+ router.delete('/', (req,res,next  ) => {
+    const collection = req.app.locals[config.dbCollection];
+    collection.remove({})
         .then(result => res.json(result))
         .catch(error => res.send(error));
-        }
-    }
- );
+  }); 
 
-router.delete('/appointments/:id', (req, res, next) => {
+router.delete('/:id', (req, res, next) => {
     const {id} = req.params;
-    const _id = ObjectOD(id);
+    const _id = ObjectId(id);
     const collection = req.app.locals[config.dbCollection];
     collection.deleteOne({_id})
     .then(result => res.json(result))
     .catch(error => res.send(error));
-    res.send('Appointment deleted');
+    
     
 });
 module.exports = router; 
